0d0ab18efa6471ff40b698d65a8096290cf99613
Julie Vogh <juvo0001@stud.kea.dk>
2024-01-31 08:11:00 +0100

    random numbers guess

--- gæt_et_tal/randomnumberguess.js
+++ gæt_et_tal/randomnumberguess.js
@@ -1,18 +1,41 @@
-
 // prompt("Guess a number between 1 and 100:");
 
 //function guessedNumber() {
-
-}
 
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
 }
 
-console.log(randomNumber(101));
+let guess = 1;
 
+// Generate a random number initially
+let currentRandomNumber = randomNumber(101);
 let returnedNumber = document.getElementById("number");
-returnedNumber.innerHTML = randomNumber(101);
+
+let tryAgain = (document.getElementById("tryagain").onclick = function () {
+  location.reload();
+});
+
+console.log(currentRandomNumber);
+
+document.getElementById("submitguess").onclick = function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  let x = document.getElementById("guessField").value;
+  let resultText = document.getElementById("resultText");
+
+  if (parseInt(x) === currentRandomNumber) {
+    resultText.innerHTML = "You guessed it right in " + guess + " GUESSES!";
+  } else if (parseInt(x) < currentRandomNumber) {
+    guess++;
+    resultText.innerHTML = "Try a higher number!";
+  } else if (parseInt(x) > currentRandomNumber) {
+    guess++;
+    resultText.innerHTML = "Try a lower number!";
+  }
+};
+
+/*
 
 let guessedNumber = "";
 
@@ -25,3 +48,4 @@ if (randomNumber === parseInt(guessedNumber)) {
 } else if (randomNumber > parseInt(guessedNumber)) {
   console.log("Too low!");
 }
+*/
