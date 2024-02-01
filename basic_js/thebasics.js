e4e524cb870fd5d817caecb3f6024702a3694c07
Julievogh <juvo0001@stud.kea.dk>
2024-02-01 20:02:12 +0100

    areal or sq

--- basic_js/thebasics.js
+++ basic_js/thebasics.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/*
+
+Øvelser
+
 const bool = true;
 const num = 41;
 const str = "Julie";
@@ -13,6 +17,7 @@ const sym = Symbol("symbol");
 
 /* Brug typeof til at finde ud af typen af en værdi. */
 
+/*
 let hej = "prut";
 
 const n1 = 1;
@@ -49,5 +54,83 @@ function presentPet(firstName, animalType, animalName) {
 
 presentPet("Julie", "dog", "Mozart");
 
-
 console.log("Helllo", presentPet);
+
+
+/*
+
+Original:
+
+function greeting(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const result = greeting("Klaus");
+console.log(result);
+*/
+
+/*
+
+test 
+
+function greeting(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const result = greeting("Klaus");
+const txt = `Greeting is ${greeting("a name")}`;
+console.log(greeting("any name"));
+*/
+
+// Areal af rektangel
+/*
+function udregnRektangelAreal(width, height) {
+  let rektangelResult = width * height;
+
+  console.log(`Arealet af en rektangel er  ${rektangelResult}`);
+}
+
+udregnRektangelAreal(500, 200);
+*/
+// ELLER
+
+function udregnRektangelAreal(width, height) {
+  let rektangelResult = width * height;
+  return rektangelResult;
+}
+
+let resultatRAreal = udregnRektangelAreal(700, 200);
+
+console.log(`Arealet af en rektangel er  ${resultatRAreal}`);
+
+//
+// fra msquaremeter til squarefeet
+
+function convertSquareMToSquareFit(squareMeter, squareFeet = 10.7639104) {
+  let result = squareMeter * squareFeet;
+
+  console.log(
+    `if you have ${squareMeter} square meter you have ${result} square feet`
+  );
+}
+
+convertSquareMToSquareFit(23);
+
+// ELLER:
+
+/*
+
+
+function convertSquareMToSquareFit(squareMeter, squareFeet = 10.7639104) {
+  let result = squareMeter * squareFeet;
+  return result; returned to the caller
+}
+
+areaInSquareFeet
+let areaInSquareFeet = convertSquareMToSquareFit(23);
+
+
+console.log(`If you have 23 square meters, you have ${areaInSquareFeet} square feet`);
+
+
+*/
