d446acfea90ce02ba168523fb0d990119b94820b
Julievogh <juvo0001@stud.kea.dk>
2024-02-10 10:38:27 +0100

    khhj

--- profanity_filter/profanity.js
+++ profanity_filter/profanity.js
@@ -1,8 +1,34 @@
-
-
-
 const curseWords = [
   { bad: "var", good: "const" },
   { bad: "float", good: "grid" },
   { bad: "marquee", good: "just don't" },
 ];
+
+function goodToBadCurse(text) {
+  let newText = text;
+  curseWords.forEach(function (pair) {
+    newText = newText.replaceAll(pair.bad, pair.good);
+  });
+  return newText;
+}
+
+console.log(goodToBadCurse());
+
+/*
+let theText =
+  "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+
+let newText =
+  "Programmering er sjovt! Men husk altid at bruge const, grid og just don't i moderne kodning.";
+*/
+let findWord = theText.replaceAll("ord", "ting");
+
+console.log(findWord);
+
+/*Find ordet "ord" i strengen theText og erstat den med ting 
+
+Prøv med split / join.
+
+Prøv med replaceAll()
+
+console.log dit resultat*/
