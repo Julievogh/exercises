d3f981bb5511cadc68a7bee650042486a2c220e2
Julie Vogh <juvo0001@stud.kea.dk>
2024-02-15 15:43:58 +0100

    start på to do efter failed netflix btn

---
+++ netflix2/netflix.js
@@ -0,0 +1,4 @@
+document.getElementById("nextButton").addEventListener("click", function () {
+  // Add your logic here for what happens when the button is clicked
+  console.log("Next episode clicked!");
+});
